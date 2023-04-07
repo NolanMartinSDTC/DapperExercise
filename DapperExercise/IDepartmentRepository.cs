8b15a1ad9b61848eaa9e2b75d63892709c0d4818
Nolan Martin <nmartinsdtc@gmail.com>
2023-04-07 10:20:43 -0600

    exercise 1: get all departments, and insert to departments

---
+++ DapperExercise/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DapperExercise
+{
+	public interface IDepartmentRepository
+	{
+		IEnumerable<Department> GetAllDepartments();
+		//IEnumerable<Department> InsertDepartments();
+	}
+}
+
